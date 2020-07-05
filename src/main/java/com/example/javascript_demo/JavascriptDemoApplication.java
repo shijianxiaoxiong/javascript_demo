d5b875ad38cf70b2825f8bbadbd9cb341485c9df
shijianxiaoxiong <782599753@qq.com>
2020-07-05 22:22:55 +0800

    first commit

---
+++ src/main/java/com/example/javascript_demo/JavascriptDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascript_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javascript_demo/JavascriptDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascript_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
